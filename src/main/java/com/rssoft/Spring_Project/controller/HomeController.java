4058d65a4846066ccf21030889c7eaffb8719e46
Harshal-Meher <harshalmeher294@gmail.com>
2025-05-08 17:27:56 +0530

    CRUD

---
+++ src/main/java/com/rssoft/Spring_Project/controller/HomeController.java
@@ -0,0 +1,1 @@
+package com.rssoft.Spring_Project.controller;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.RequestMapping;@Controllerpublic class HomeController {    @RequestMapping("/")    public String home(){        return "index";    }}
